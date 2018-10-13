11d129355970cf10144bf9545ce52bd4c7d475b2
despaink <43147974+despaink@users.noreply.github.com>
2018-10-13 10:16:56 -0600

    Add files via upload

---
+++ lab-3-angular-despaink-master/lab3/avatar.js
@@ -0,0 +1,47 @@
+/*global angular*/
+angular.module('app', [])
+  .controller('mainCtrl', mainCtrl)
+  .directive('avatar', avatarDirective);
+
+function mainCtrl ($scope) {
+
+  $scope.users = [];
+
+  $scope.addNew = function (user) {
+    $scope.users.push({ 
+      name: user.name,
+      email: user.email,
+      avatarUrl: user.url
+    }); /* [1] */
+    
+    user.name = ''; /* [2] */
+    user.email = '';
+    user.url = '';
+  };
+}
+
+function avatarDirective () {
+  return {
+    scope: {
+
+      user: '=' /* [1] */
+    },
+    restrict: 'E', /* [2] */
+    replace: 'true',
+    template: (
+      '<div class="Avatar">' +
+        '<img ng-src="{{user.avatarUrl}}" />' +
+        '<h4>{{user.name}}</h4>' +
+        '<h5>{{user.email}}</h5>'+ 
+      '</div>'
+    ), /* [3] */
+    link: link
+  };
+  
+  function link (scope) { /* [4] */
+    if (!scope.user.avatarUrl) {
+      scope.user.avatarUrl = 'https://www.drupal.org/files/issues/default-avatar.png';
+    }
+  }
+
+}
